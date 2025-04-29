330022a008d30ebcf044a3835dd01865b835694e
Alexandre GARCIN <perso@alex-home.fr>
2025-04-29 17:51:47 +0200

    Add APP_DIR BUILD_TARGET and DEPENDS options to ts_app

--- cmake/ts_app.cmake
+++ cmake/ts_app.cmake
@@ -1,13 +1,22 @@
 function(ts_app)
    set(options)
-   set(oneValueArgs TARGET_NAME)
-   set(multiValueArgs)
+   set(oneValueArgs TARGET_NAME APP_DIR BUILD_TARGET)
+   set(multiValueArgs DEPENDS)
    cmake_parse_arguments(PARSE_ARGV 0 arg
       "${options}" "${oneValueArgs}" "${multiValueArgs}"
    )
 
-   file(GLOB_RECURSE ASSETS 
-      ${CMAKE_CURRENT_SOURCE_DIR}/src/*
+
+   if (DEFINED arg_APP_DIR)
+      file(RELATIVE_PATH OUTPUT_DIR ${CMAKE_CURRENT_SOURCE_DIR} ${arg_APP_DIR})
+      set(OUTPUT_DIR ${CMAKE_CURRENT_BINARY_DIR}/${OUTPUT_DIR}/dist)
+   else()
+      set(OUTPUT_DIR "${CMAKE_CURRENT_BINARY_DIR}/dist")
+      set(arg_APP_DIR "${CMAKE_CURRENT_SOURCE_DIR}/src")
+   endif()
+
+   file(GLOB_RECURSE ASSETS
+      ${arg_APP_DIR}/*
       ${CMAKE_CURRENT_SOURCE_DIR}/.env.*
       ${CMAKE_CURRENT_SOURCE_DIR}/build.ts
       ${CMAKE_CURRENT_SOURCE_DIR}/index.html
@@ -15,19 +24,32 @@ function(ts_app)
       ${CMAKE_CURRENT_SOURCE_DIR}/tailwind.config.ts
       ${CMAKE_CURRENT_SOURCE_DIR}/tsconfig.json
    )
+   
+   if (DEFINED arg_DEPENDS)
+      file(GLOB_RECURSE ASSETS_
+         ${arg_DEPENDS}/*
+      )
+
+      list(APPEND ASSETS ${ASSETS_})
+   endif()
+
+   if (NOT DEFINED arg_BUILD_TARGET)
+      set(arg_BUILD_TARGET "build")
+   endif()
+
 
    get_target_property(DEPS alx-home_ts_utils DEPS)
    add_custom_command(
-      OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/dist/index.html
+      OUTPUT ${OUTPUT_DIR}/index.html
       DEPENDS 
          alx-home_ts_utils
          ${DEPS}
          ${ASSETS}
       WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
-      COMMAND npm run build
+      COMMAND npm run "${arg_BUILD_TARGET}"
    )
 
-   list(APPEND DEPS ${CMAKE_CURRENT_BINARY_DIR}/dist/index.html)
+   list(APPEND DEPS ${OUTPUT_DIR}/index.html)
    add_custom_target(${arg_TARGET_NAME}
       DEPENDS 
          alx-home_ts_utils
@@ -35,7 +57,7 @@ function(ts_app)
    )
 
    set_target_properties(${arg_TARGET_NAME} PROPERTIES
-      FOLDER ${CMAKE_CURRENT_BINARY_DIR}/dist/
+      FOLDER ${OUTPUT_DIR}/
       DEPS "${DEPS}"
    )
 endfunction(ts_app)
